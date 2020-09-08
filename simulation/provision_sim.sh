4595e9b80b92b8a2cf8dceef5b613cb35fe8cfe1
Fredrik Baberg <fredrik.baberg@gmail.com>
2020-09-09 00:54:26 +0200

    Set name and hostname

--- simulation/provision_sim.sh
+++ simulation/provision_sim.sh
@@ -1,13 +1,13 @@
 # Source ros
-source /opt/ros/indigo/setup.bash
+source /opt/ros/kinetic/setup.bash
 # Setup Gazebo sources
 sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
 wget https://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
 # Setup workspace
 mkdir -p $HOME/catkin_ws/src
 # Clone git repos
-git clone https://github.com/mas-group/youbot_simulation.git --single-branch --branch indigo-devel $HOME/catkin_ws/src/youbot_simulation
-git clone https://github.com/mas-group/youbot_description.git --single-branch --branch indigo-devel $HOME/catkin_ws/src/youbot_description
+git clone https://github.com/mas-group/youbot_simulation.git --single-branch --branch kinetic-devel $HOME/catkin_ws/src/youbot_simulation
+git clone https://github.com/mas-group/youbot_description.git --single-branch --branch kinetic-devel $HOME/catkin_ws/src/youbot_description
 git clone https://github.com/Kinovarobotics/kinova-ros.git --single-branch --branch master $HOME/catkin_ws/src/kinova-ros
 mkdir -p ~/.gazebo && git clone https://github.com/osrf/gazebo_models --single-branch --branch master $HOME/.gazebo/models && rm -rf ~/.gazebo/models/.git
 git clone https://github.com/siemens/ros-sharp.git --single-branch --branch master /tmp/ros-sharp && cp -R /tmp/ros-sharp/ROS/ $HOME/catkin_ws/src/ros-sharp # && rm -rf /tmp/ros-sharp
@@ -19,20 +19,20 @@ touch $HOME/catkin_ws/src/kinova-ros/kinova_moveit/kinova_arm_moveit_demo/CATKIN
 # sed -i 's+hardware_interface/++g' $HOME/catkin_ws/src/kinova-ros/kinova_description/urdf/kinova_common.xacro
 # Install additional packages
 sudo apt update
-sudo apt install -y linux-headers-$(uname -r) # xfce4 slim
+sudo apt install -y linux-headers-$(uname -r)
 sudo apt install -y \
-ros-indigo-xacro \
-ros-indigo-gazebo-ros-control \
-ros-indigo-effort-controllers \
-ros-indigo-joint-state-controller \
-ros-indigo-joint-trajectory-controller \
-ros-indigo-ros-control \
-ros-indigo-ros-controllers \
-ros-indigo-gazebo-plugins \
-ros-indigo-gazebo-ros-pkgs \
-ros-indigo-rosbridge-server \
-ros-indigo-turtlebot-gazebo \
-ros-indigo-rqt-graph
+ros-kinetic-xacro \
+ros-kinetic-gazebo-ros-control \
+ros-kinetic-effort-controllers \
+ros-kinetic-joint-state-controller \
+ros-kinetic-joint-trajectory-controller \
+ros-kinetic-ros-control \
+ros-kinetic-ros-controllers \
+ros-kinetic-gazebo-plugins \
+ros-kinetic-gazebo-ros-pkgs \
+ros-kinetic-rosbridge-server \
+ros-kinetic-turtlebot-gazebo \
+ros-kinetic-rqt-graph
 # Install packages using rosdep
 cd $HOME/catkin_ws
 rosdep install --from-path src --ignore-src -ry
@@ -40,4 +40,4 @@ rosdep install --from-path src --ignore-src -ry
 sudo apt-get clean
 sudo rm -rf /var/lib/apt/lists/*
 # Add line to source workspace
-# sudo -H -u vagrant bash -c "grep -q -F 'source /opt/ros/indigo/setup.bash' ~/.bashrc || echo 'source $HOME/catkin_ws/devel/setup.bash --extend' >> ~/.bashrc"
+# sudo -H -u vagrant bash -c "grep -q -F 'source /opt/ros/kinetic/setup.bash' ~/.bashrc || echo 'source $HOME/catkin_ws/devel/setup.bash --extend' >> ~/.bashrc"
--- virtualbox-builder/provision.sh
+++ virtualbox-builder/provision.sh
@@ -1,10 +1,10 @@
 # Install and setup Virtualbox
-wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
-wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
-echo "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib" | sudo tee -a /etc/apt/sources.list.d/virtualbox.list
+# wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+# wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+# echo "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib" | sudo tee -a /etc/apt/sources.list.d/virtualbox.list
 apt-get update
 apt-get -y install \
-virtualbox-6.1 \
+virtualbox \
 vagrant \
 xinit \
 i3 \
